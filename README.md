591017338991217c75913ee572d5cd46b83e7439
Xavier Lange <xrlange@gmail.com>
2011-10-15 06:20:37 +0000

    Why in the WORLD does this compile and not fail assertion\?

---
+++ README.md
@@ -0,0 +1,10 @@
+Let's break opensplice.
+
+  libtoolize -c || glibtoolize -c
+  autoreconf -fv --install
+  ./configure
+  make
+
+Existing test tools
+
+  userqot
--- src/userqos.cpp
+++ src/userqos.cpp
@@ -1,8 +1,21 @@
+#include <stdlib.h>
+#include <assert.h>
+
 #include <ccpp_dds_dcps.h>
 
 int main(int argc, char** args){
   // DomainParticipantFactory_var dpf = DDS::DomainParticipantFactory::get_instance();
-  DDS::DomainParticipant *participant = DDS::DomainParticipantFactory::get_instance()->create_participant(0,PARTICIPANT_QOS_DEFAULT,NULL,0);
-
+  DDS::DomainParticipantFactory_var dpf = DDS::DomainParticipantFactory::get_instance();
+  assert( NULL != dpf._retn());
+  assert( NULL != &dpf );
+  //            DomainParticipant_var participant;
+  DDS::DomainParticipant_var participant = dpf->create_participant(NULL,
+                                                                   PARTICIPANT_QOS_DEFAULT,
+                                                                   NULL,
+                                                                   DDS::STATUS_MASK_NONE);
+  assert( NULL == participant );
+  assert( NULL != participant._retn() ); 
+  
+//  participant._retn()->delete_participant();
   return 0;
 }
