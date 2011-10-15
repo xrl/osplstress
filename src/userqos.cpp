7fec6bd1ddf8c8156c8c547589e22f3a66671486
Xavier Lange <xrlange@gmail.com>
2011-10-15 01:40:46 +0000

    Whoa, compiles with opensplice

--- src/userqos.cpp
+++ src/userqos.cpp
@@ -1,5 +1,8 @@
 #include <ccpp_dds_dcps.h>
 
 int main(int argc, char** args){
+  // DomainParticipantFactory_var dpf = DDS::DomainParticipantFactory::get_instance();
+  DDS::DomainParticipant *participant = DDS::DomainParticipantFactory::get_instance()->create_participant(0,PARTICIPANT_QOS_DEFAULT,NULL,0);
+
   return 0;
 }
