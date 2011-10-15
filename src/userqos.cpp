c3c3c98ea11f188176dcfee55173742d66057801
Xavier Lange <xrlange@gmail.com>
2011-10-15 11:14:45 +0000

    Now settings user qos

--- src/userqos.cpp
+++ src/userqos.cpp
@@ -28,6 +28,7 @@ int main(int argc, char** args){
     I repeat, MUST, add #pragma. Why would you ever NOT want the TypeSupport? Who knows.
     OpenSplice_PreProcessor_usermanual.pdf , Section: 1.5.1 KeyDefinitions
   **/
+  // Memory leak: I cannot seem to free pid_ts. Delete is blocked nor can I find _release per the docs
   PID::PresenceTypeSupport_ptr pid_ts = new PID::PresenceTypeSupport();
   assert( NULL != pid_ts );
   retval = pid_ts->register_type(participant, pid_ts->get_type_name());
@@ -42,7 +43,11 @@ int main(int argc, char** args){
                                                             topic_qos,
                                                             NULL,
                                                             DDS::STATUS_MASK_NONE); 
+  assert( NULL != presence_topic._retn() );
 
+  /**
+     Publisher code
+  **/
   DDS::PublisherQos publisher_qos;
   retval = participant->get_default_publisher_qos(publisher_qos);
   assert( DDS::RETCODE_OK == retval );
@@ -51,6 +56,16 @@ int main(int argc, char** args){
                                                                DDS::STATUS_MASK_NONE);
   assert( NULL != publisher._retn() );
 
+  /**
+    PID Data Writer
+  **/
+  DDS::DataWriterQos dw_qos;
+  publisher->get_default_datawriter_qos(dw_qos);
+  const char* msg = "HI THERE COWBOY\n";
+  dw_qos.user_data.value = DDS_DCPSUFLSeq<unsigned char, DDS::octSeq_uniq_>(msg);
+  assert( strlen(msg) == dw_qos.user_data.value.length() );
+  publisher->create_datawriter(presence_topic, dw_qos, NULL, DDS::STATUS_MASK_NONE); 
+  
   retval = dpf->delete_participant(participant);
   assert( DDS::RETCODE_OK == retval );
   return 0;
